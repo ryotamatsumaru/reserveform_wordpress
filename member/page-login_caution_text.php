8218a5aecf3687b1a40646985a84ff0a197b8e47
ryotamatsumaru <ryota.matsumaru@gmail.com>
2024-10-17 15:55:41 +0900

    Update page-login_caution_text.php

--- member/page-login_caution_text.php
+++ member/page-login_caution_text.php
@@ -71,27 +71,41 @@ if(!empty($_POST['id'])){
 
     <nav id="navi">
       <ul class="navi-area wrapper">
-        <li><a href="http://demohotel.local"><span class="en">Top</span>
-            <p class="ja">トップページ</p>
-          </a></li>
-        <li><a href="http://demohotel.local/concept"><span class="en">Concept</span>
-            <p class="ja">当館の特徴</p>
-          </a></li>
-        <li><a href="http://demohotel.local/fasility"><span class="en">Fasility</span>
-            <p class="ja">施設案内</p>
-          </a></li>
-        <li><a href="http://demohotel.local/restaurant"><span class="en">Restaurant</span>
-            <p class="ja">レストラン</p>
-          </a></li>
-        <li><a href="http://demohotel.local/roomlist"><span class="en">Room Type</span>
-            <p class="ja">お部屋一覧</p>
-          </a></li>
-        <li><a href="http://demohotel.local/#news"><span class="en">News</span>
-            <p class="ja">お知らせ</p>
-          </a></li>
-        <li><a href="http://demohotel.local/member"><span class="en">Member</span>
-            <p class="ja">会員制度</p>
-          </a></li>
+        <li>
+          <a href="http://demohotel.local"><span class="en">Top</span>
+          <p class="ja">トップページ</p>
+          </a>
+        </li>
+        <li>
+          <a href="http://demohotel.local/concept"><span class="en">Concept</span>
+          <p class="ja">当館の特徴</p>
+          </a>
+        </li>
+        <li>
+          <a href="http://demohotel.local/fasility"><span class="en">Fasility</span>
+          <p class="ja">施設案内</p>
+          </a>
+        </li>
+        <li>
+          <a href="http://demohotel.local/restaurant"><span class="en">Restaurant</span>
+          <p class="ja">レストラン</p>
+          </a>
+        </li>
+        <li>
+          <a href="http://demohotel.local/roomlist"><span class="en">Room Type</span>
+          <p class="ja">お部屋一覧</p>
+          </a>
+        </li>
+        <li>
+          <a href="http://demohotel.local/#news"><span class="en">News</span>
+          <p class="ja">お知らせ</p>
+          </a>
+        </li>
+        <li>
+          <a href="http://demohotel.local/member"><span class="en">Member</span>
+          <p class="ja">会員制度</p>
+          </a>
+        </li>
       </ul>
     </nav>
   </header>
